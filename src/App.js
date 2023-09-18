e182a6d0893684e6c2a5b51d4058d8f16dea23d3
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-09-18 22:09:48 +0530

    react components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PartOne from './components/PartOne';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This is my react project</h1>
+     <PartOne/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PartOne.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function PartOne() {
+  return (
+    <div>This is my Components</div>
+  )
+}
+  
