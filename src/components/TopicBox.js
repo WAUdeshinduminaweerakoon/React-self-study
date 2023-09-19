e8e2584521d98265f640fa7be59f522a7b7cb286
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-09-19 20:22:22 +0530

    Dynamic components, and how to use

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -2,16 +2,20 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox()  {
-    const topic = "My Favourite food Kuttu";
-    const item = "and rice"
-
+    // const topic = "My Favourite food Kuttu";
+    // const item = "and rice"
+    const a = 10;
+    const b = 26;
+    const c = a+b;
+ 
 
   return (
     <div>
         <div className="topicBox">
-            <span className='text'>{topic} {item}</span>
+            {/* <span className='text'>{topic} {item}</span> */}
+            <span className='text'>{c}{alert("hellow")}</span>
         </div>
     </div>
   )
 }
-
+// {} athule onama Js code ekak type kara hakiya. object and Boolean not using
