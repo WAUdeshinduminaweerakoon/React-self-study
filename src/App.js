89b399c8a04cbfff22ab231a4bc3e68ca162a3dc
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-09-25 16:31:46 +0530

    uesing useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 
 import './App.css';
 
-import Home from './Pages/Home';
-import { BrowserRouter as Router,Routes, Route  } from 'react-router-dom';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
+// import Home from './Pages/Home';
+// import { BrowserRouter as Router,Routes, Route  } from 'react-router-dom';
+// import About from './Pages/About';
+// import Contact from './Pages/Contact';
+import { useState } from 'react';
 
 
 function App() {
+  const [count, setCount]= useState(0);
+
+  //let count = 0;
+
+  const increment = () =>{
+   // count +=1;
+   setCount(count+1);
+  }
+  const decrement = () =>{
+    //count -=1;
+    setCount(count-1);
+  }
   return (
-    <Router>
-      <div>
-        <Routes>
-            <Route exact path='/' element={< Home />}></Route>
-            <Route exact path='/About' element={< About />}></Route>
-            <Route path='/Contact' element={<Contact/>}></Route>
-              
-           
-            
-      </Routes>
-      </div>
-   </Router>
+  //   <Router>
+  //     <div>
+  //       <Routes>
+  //           <Route exact path='/' element={< Home />}></Route>
+  //           <Route exact path='/About' element={< About />}></Route>
+  //           <Route path='/Contact' element={<Contact/>}></Route>         
+  //     </Routes>
+  //     </div>
+  //  </Router>
+  <>
+  <span className='title'>My Counter</span>
+  <p className='subTitle'> The count {count} </p>
+  <button onClick={decrement} className='button'>-</button>
+  <button onClick={increment} className='button'>+</button>
+  </>
+  
+
   );
 }
 
